0820b7195453e8bf55940709583cdf18cb245847
kingdido999 <info@desmonding.me>
2018-11-20 09:27:44 +0800

    Initial Commit

---
+++ beefalo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  beefalo
+//
+//  Created by Desmond Ding on 11/20/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
